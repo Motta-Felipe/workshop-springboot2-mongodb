de46169f52ed52176b92fc787c69d6a5fb9d5f29
Motta-Felipe <flp.motta@hotmail.com>
2021-05-05 14:42:06 +0200

    Post by Id

---
+++ src/main/java/com/felipemotta/workshopmongo/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.felipemotta.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipemotta.workshopmongo.domain.Post;
+import com.felipemotta.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value= "/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService service;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity <Post> findbyId(@PathVariable String id){
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ src/main/java/com/felipemotta/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.felipemotta.workshopmongo.services;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipemotta.workshopmongo.domain.Post;
+import com.felipemotta.workshopmongo.repository.PostRepository;
+import com.felipemotta.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
